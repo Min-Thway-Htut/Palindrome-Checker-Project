a0bb3ac0af22027d192699a979f878fcff3a38c9
Min-Thway-Htut <minthwayhtut68@gmail.com>
2024-07-24 13:16:45 +0200

    Adding javaScript to the project

--- script.js
+++ script.js
@@ -2,5 +2,9 @@ function myFunction(){
     var textInput = document.getElementById("text-input").value;
     if (textInput.trim() === ''){
         alert("Please input a value");
+    }else if (textInput === "A"){
+        document.getElementById('result').innerHTML = "A is a palindrome"
+    }else if (textInput === "eye"){
+        document.getElementById('result').innerHTML = "eye is a palindrome"
     }
 }
